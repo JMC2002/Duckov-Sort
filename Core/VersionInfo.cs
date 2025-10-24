a5bd95783358dd643c37bbbc118727eb749c749b
MC J <jmc8@qq.com>
2025-10-24 16:25:11 +0800

    修订版本号

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -7,7 +7,7 @@ namespace DuckSort.Core
     public static class VersionInfo
     {
         public const string Name = "DuckSort";
-        public const string Version = "1.0.2";
+        public const string Version = "1.1.6";
 
         public static string Tag => $"[{Name} v{Version}]";
     }
