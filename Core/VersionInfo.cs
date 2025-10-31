a01b343aae1b06eacb0ff50404431127f2bea541
MC J <jmc8@qq.com>
2025-10-31 11:09:59 +0800

    修复一些本地化文本

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@
     public static class VersionInfo
     {
         public const string Name = "DuckSort";
-        public const string Version = "1.7.1";
+        public const string Version = "1.7.2";
 
         public static string Tag => $"[{Name} v{Version}]";
     }
